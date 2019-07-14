eccbe06c604342805bcde0e9fd7c7c2425584613
Zoey Fryhover <znfryhover@gmail.com>
2019-07-14 19:21:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]}  "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]}  "
+end
